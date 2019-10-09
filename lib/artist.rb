b333dc0f5c05be76410bee534798d13b621d8e6d
Vanna Winland <vgwinland@gmail.com>
2019-10-09 02:23:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,6 +25,6 @@ class Artist
   end
   
   def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create_by_name(name)
+    self.find(name) || self.create_by_name(name)
   end
 end
