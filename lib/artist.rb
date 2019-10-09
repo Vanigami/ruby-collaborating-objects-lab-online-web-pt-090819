7f4d25b36a6a5cdf4de51aec65cbc19b1b501f66
Vanna Winland <vgwinland@gmail.com>
2019-10-09 01:59:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,32 +1,5 @@
 class Artist 
   attr_accessor :name 
   
-  @@all = [] 
-  
   def initialize(name)
-    @name = name
-    @songs = []
-  end
-  
-  def self.all
-    @@all
-  end
-  
-  def add_song(song)
-   @songs << song  
-  end
- 
-  def self.create_by_name(name)
-    song = self.create 
-    song.name = name 
-    song
-  end
-  
-  def self.find_by_name(name)
-    self.all.find{|song| song.name == name}
-  end
-  
-  def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create_by_name(name)
-  end
-end
+    @na
