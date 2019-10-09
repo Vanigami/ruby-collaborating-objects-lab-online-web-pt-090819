6be66031cf124f1ffa78f198984cfccb166acd3f
Vanna Winland <vgwinland@gmail.com>
2019-10-09 02:08:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,11 @@
 class Artist 
-  attr_accessor :name 
+  attr_accessor :name, :songs
   
   @@all = [] 
   
   def initialize(name)
     @name = name 
-    @@all << self
+    @songs << self
   end
   
   def self.all 
