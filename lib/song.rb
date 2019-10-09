fb7bd7dff4bfb86e12ba9a56c72e30f7aa7fc169
Vanna Winland <vgwinland@gmail.com>
2019-10-09 02:47:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,9 +13,10 @@ class Song
     @@all 
   end
   
-  def self.new_by_filename
+  def self.new_by_filename(file)
     artist_name = file.split(" - ")[0]
     song_name = file.split(" - ")[1]
+    
   end
   
   def artist_name=(name)
